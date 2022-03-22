f0156854004fa0e5332199f02aa8ff53d1a2030d
Henrique da Silva Sousa <86082129+HenriquedSS1912@users.noreply.github.com>
2022-03-22 19:42:39 -0300

    Update dados_email.py

--- dados_email.py
+++ dados_email.py
@@ -1,2 +1,5 @@
 meu_email = 'seu_email'
 minha_senha = 'sua senha'
+
+# Lembrete: desative a opção Aplicativo menos seguro, link: Aplicativo menos seguro
+# link: https://myaccount.google.com/lesssecureapps
